7feee5a4044c8832ab9801c37aaa16dccc480b1a
papabn <pndiaye999@gmail.com>
2024-04-26 14:48:11 +0000

    Add file upload functionality to UserController.php and add new user CV file

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,11 +15,19 @@ class UserController extends Controller
         if (!$user) {
             return response()->json(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
         }
+        if ($request->hasFile('cv')) {
+            $cv = $request->file('cv');
+            $extension = $cv->getClientOriginalExtension();
+            $cvName = time() . '.' . $extension;
+            $cv->move('AllCv', $cvName);
+        } else {
+            $cvName = null;
+        }
         $user->update([
             'name' => $request->name,
             'adresse' => $request->adresse,
             'tarif' => $request->tarif,
-            'cv' => $request->cv,
+            'cv' => $cvName,
             'telephone' => $request->telephone,
             // 'solde' => $request->solde,
         ]);
